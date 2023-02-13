e2ec127ac3aa37caef2f503f453b77aceb8a764d
Karla GaSa <karlagsanabria@gmail.com>
2023-02-13 09:16:07 -0600

    tests

--- test/md-link.spec.js
+++ test/md-link.spec.js
@@ -12,7 +12,7 @@ describe('mdLinks', () => {
   it('debe rechazar la promesa si no encuentra un path', () => mdLinks('noexiste/path.md').catch((error) => {
     expect(error).toStrictEqual(new Error('La ruta no existe'));
   }));
-  it('debe rechazar la promesa si el archivo no es .md', () => mdLinks('./Pruebas/Sin-links.txt').catch((error) => {
+  it('debe rechazar la promesa si el archivo no es .md', () => mdLinks('./Pruebas/Sin-links.md').catch((error) => {
     expect(error).toStrictEqual(new Error('El archivo no es .md'));
   }));
   it('debe rechazar la promesa si el archivo no contiene links', () => mdLinks('./Pruebas/Sin-links.txt').catch((error) => {
